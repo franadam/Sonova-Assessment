ff83c0b7eae8ad555b1c5ecd1ff9dd802ef68a2f
francois <francoisadam@hotmail.be>
2021-07-13 19:38:13 +0200

    style: add image to Footer poster

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import classes from './Footer.module.css';
+import { FaQuoteLeft } from 'react-icons/fa';
 
 const Footer = (): JSX.Element => {
   const year = new Date().getFullYear();
   return (
     <div className={classes.container}>
-      <div className={classes.poster}></div>
+      <div className={classes.poster}>
+        <FaQuoteLeft className={classes.icon} />
+        <div className={classes.citation}>
+          <div className={classes.text}>
+            GEERS, DIE VERSTEHEN WAS VOM HÖREN.
+          </div>
+          <div className={classes.author}>Thomas Gottschalk</div>
+        </div>
+      </div>
       <div className={classes.copyright}>
-        Copyright © {year}. All rights reserved.
+        Copyright © {year} Sonova AG. All rights reserved.
       </div>
     </div>
   );
